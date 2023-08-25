fe0e921acb0035d0b5ffd76ba3717dda08ff197b
zl <jackiejay888@gmail.com>
2023-08-26 00:50:33 +0800

    app_parser.py

--- script/app_parser.py
+++ script/app_parser.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python
 '''
-Created on 2023/08/22
+Created on 2023/08/24
 
 @author: ZL Chen
-@title: Web parser
+@title: APP parser
 '''
 
-from selenium import webdriver
+import unittest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
 
-driver = webdriver.Chrome()
-# driver.get('https://iwa.chiconypower.com/')
-driver.get('https://www.google.com.tw/')
-driver.close()
+capabilities = dict(
+	platformName='Android',
+	automationName='uiautomator2',
+	deviceName='Android',
+	appPackage='com.android.settings',
+	appActivity='.Settings',
+    language='en',
+    locale='US'
+)
 
+appium_server_url = 'http://localhost:4723'
+
+class TestAppium(unittest.TestCase):
+	def setUp(self) -> None:
+		self.driver = webdriver.Remote(appium_server_url, capabilities)
+
+	def tearDown(self) -> None:
+		if self.driver:
+			self.driver.quit()
+
+	def test_find_battery(self) -> None:
+		el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Wi-Fi"]')
+		el.click()
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
